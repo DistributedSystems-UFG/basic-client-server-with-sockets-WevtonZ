d9c674fb9468cac4ac036d1fe52e9c31ed8a2540
Wevton Junior <wevtonj@gmail.com>
2026-03-23 18:54:51 -0300

    Update server to perform arithmetic operations
    Refactor server to handle basic arithmetic operations based on received messages.

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
 from socket  import *
 from constCS import * #-
 
-s = socket(AF_INET, SOCK_STREAM) 
+s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
+(conn, addr) = s.accept()  # returns new socket and addr. client
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  if not data: break	   # stop if client stopped
+  msg = bytes.decode(data)
+  tpl = eval(msg)
+  n1 = tpl[0]
+  n2 = tpl[1]
+  op = tpl[2]
+
+  res = 0 # guardar resultado
+
+  if op == 'a':
+    res = n1+n2
+  elif op == 'm':
+    res = n1 * n2
+  elif op == 'd':
+    res = n1 / n2
+  elif op == s:
+    res = n1-n2
+
+  conn.send(str.encode(str(res))) # return sent data plus an "*"
 conn.close()               # close the connection
